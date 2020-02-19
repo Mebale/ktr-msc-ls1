f2a61c47db775d91bdb7f40cad159ac4da907a82
Mebale <61231994+Mebale@users.noreply.github.com>
2020-02-19 12:50:48 +0100

    Create traitement.php

---
+++ traitement.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <title>Page de traitement</title>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        
+        <?php
+            echo 'Prénom : '.$_POST["user_name"].'<br>';
+            echo 'Email : ' .$_POST["user_company"].'<br>';
+            echo 'Age : ' .$_POST["user_email"].'<br>';
+            echo 'Sexe : ' .$_POST["user_telephone"].'<br>';
+        ?>
+    </body>
+</html>
